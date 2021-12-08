287c686a48bc81163507b5179512d9e9201ae874
Hung Do <hungdo@sentinel.ag>
2021-12-08 11:50:59 +1100

    fixed unit tests and bugs in api_server

--- rokt/tests/test_data_pipeline.py
+++ rokt/tests/test_data_pipeline.py
@@ -7,11 +7,11 @@ class TestProcessDf(unittest.TestCase):
     def test_process_df(self):
         data = [['2000-01-01T25:49Z', 'casey.eichmann@hayes.us', '56cc8832-9f9d-4dc5-b340-8dabc5107430'],
                 [None, None, None],
-                ['2000-01-01T50:25:49Z', None, None]
+                ['2000-01-01T23:25:49Z', 'casey.eichmann@hayes.us', None]
                 ]
         input_df = pd.DataFrame(data)
         output_df = process_df(input_df, 'test_file.txt', 'sqlite')
         print(output_df)
         actual = len(output_df)
-        expected = 0
+        expected = 1
         self.assertEqual(actual, expected)
